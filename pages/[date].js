d45eccd262a3484fe80c188663116f0a097d38b6
googol88 <ronaldfleung@gmail.com>
2021-12-31 18:04:46 -0500

    💥 chore: add error handling

--- pages/[date].js
+++ pages/[date].js
@@ -1,7 +1,11 @@
-import Head from 'next/head'
+import Head from 'next/head';
 import Main from '../components/main';
+import Error from "next/error";
 
-export default function App({ data }) {
+export default function App({ data, notFound }) {
+  if (notFound) {
+    return <Error statusCode={404} />;
+  }
   const image = data.photos[0];
   console.log(image);
   return (
@@ -27,7 +31,6 @@ export async function getStaticPaths() {
       `https://api.nasa.gov/mars-photos/api/v1/rovers/perseverance/latest_photos?api_key=${process.env.KEY}`
     ).then((r) => r.json())
   ).latest_photos[0].earth_date;
-  console.log(currentDate);
   return {
     paths: [...Array(180).keys()].map((x) => ({
       params: {
@@ -39,9 +42,13 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }) {
-  console.log(params);
-  let data = await fetch(
-	`https://api.nasa.gov/mars-photos/api/v1/rovers/perseverance/photos?api_key=${process.env.KEY}&earth_date=${params.date}&camera=navcam_left`
-  ).then((r) => r.json());
-  return { props: { data }, revalidate: 30 };
+  try {
+    let data = await fetch(
+    `https://api.nasa.gov/mars-photos/api/v1/rovers/perseverance/photos?api_key=${process.env.KEY}&earth_date=${params.date}&camera=navcam_left`
+    ).then((r) => r.json());
+    return { props: { data }, revalidate: 30 };
+  } catch (e) {
+    console.log(e);
+    return { props: { notFound: true } };
+  }
 }
